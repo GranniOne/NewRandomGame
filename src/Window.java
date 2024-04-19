0cf5a7c7cb76736a414f5e799dc238a33eaf9583
Granni <grantzaumathias@gmail.com>
2024-04-19 16:31:31 +0200

    New Test

---
+++ src/Window.java
@@ -0,0 +1,14 @@
+import java.awt.*;
+import java.io.Serial;
+
+
+public class Window extends Canvas {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+
+    public Window(int width, int height, String title) {
+
+    }
+}
